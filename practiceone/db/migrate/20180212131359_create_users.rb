9f7c3e7d6a29c2e44eb54705d94e8720ef89bc12
kushy@witmates.com <kushy@witmates.com>
2018-02-19 19:04:48 +0530

    accepted nested attributes

---
+++ practiceone/db/migrate/20180212131359_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :fname
+      t.string :lname
+      t.date :bdate
+
+      t.timestamps
+    end
+  end
+end
